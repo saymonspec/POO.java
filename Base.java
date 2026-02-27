dda990b006b0fdaaae7ed7471b392fbe035422e7
Saymon Oliveira <saymonoliveira.sousa@gmail.com>
2026-02-27 20:08:21 -0300

    Create Base.java

---
+++ Base.java
@@ -0,0 +1,53 @@
+public class Base {
+    public static void main(String[] args) {
+        
+        boolean flag = true;
+
+        
+        char ch = 'A';
+
+        
+        byte b = 12;
+
+       
+        short s = 24;
+
+        
+        int i = 257;
+
+        
+        long l = 890L; 
+
+       
+        float f = 3.1415F; 
+
+       
+        double d = 2.1828;
+
+       
+        System.out.println("flag =" + flag);
+
+        
+        System.out.println("ch =" + ch);
+        
+       
+        System.out.println("b = " + b);
+
+       
+        System.out.println("s = " + s);
+
+        
+        System.out.println("i = " + i);
+
+        
+        System.out.println("l = " + l);
+
+        
+        System.out.println("f = " + f);
+
+        
+        System.out.println("d = " + d);
+
+        
+    }
+}
